90a89a2f24f33886b79a8a8b17c416bdebdc9014
plugingexperts <e8841040>
2015-06-20 14:49:37 +0530

    first commit

---
+++ admin/class-paypal-credit-card-for-woocommerce-admin.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @class       PayPal_Credit_Card_For_WooCommerce_Admin
+ * @version	1.0.0
+ * @package	paypal-credit-card-for-woocommerce
+ * @category	Class
+ * @author      johnny manziel <phpwebcreators@gmail.com>
+ */
+class PayPal_Credit_Card_For_WooCommerce_Admin {
+
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $plugin_name    The ID of this plugin.
+     */
+    private $plugin_name;
+
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
+
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string    $plugin_name       The name of this plugin.
+     * @param      string    $version    The version of this plugin.
+     */
+    public function __construct($plugin_name, $version) {
+
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+    }
+
+    public function init_paypal_credit_cart_pro() {
+        if (class_exists('WC_Payment_Gateway')) {
+            require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-paypal-credit-card-for-woocommerce-paypalextension.php';
+        }
+    }
+    
+    public function paypal_credit_card_for_woocommerce_standard_parameters() {
+        if( isset($paypal_args['BUTTONSOURCE']) ) {
+            $paypal_args['BUTTONSOURCE'] = 'mbjtechnolabs_SP';
+        } else {
+            $paypal_args['bn'] = 'mbjtechnolabs_SP';
+        }
+        return $paypal_args;
+    }
+
+}
